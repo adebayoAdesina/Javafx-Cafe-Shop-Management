1c31a4dd56f0a19b290f3093de257c36b397ce53
Adebayo Adesina <adebayoadesina789@gmail.com>
2023-10-17 21:53:19 +0100

    Log in function and query added.

--- src/main/java/com/example/cafeshopmanagement/Controller/LoginController.java
+++ src/main/java/com/example/cafeshopmanagement/Controller/LoginController.java
@@ -36,8 +36,8 @@ public class LoginController implements Initializable {
     public Button side_create_account_button;
     public Button side_already_have_an_account;
 
-    private Connection connection;
-    private PreparedStatement preparedStatement;
+    private Connection connection = null;
+    private PreparedStatement preparedStatement = null;
     private ResultSet resultSet;
     private final String[] questionList = {
             "What is your favorite Color?",
@@ -48,56 +48,97 @@ public class LoginController implements Initializable {
     ObservableList<String> observableList = FXCollections.observableArrayList(questionList);
 
     private Alert alert;
+
+    public void loginAction() {
+
+        if (login_username.getText().isEmpty() || login_password.getText().isEmpty()) {
+            fillAllFieldError();
+        } else if (login_password.getText().length() < 8) {
+            invalidPassword();
+        } else {
+            String confirmIfTrue = "SELECT username, password FROM Employee WHERE username = ? and password = ? ";
+            connection = Database.connectionDB();
+            try {
+                preparedStatement = connection.prepareStatement(confirmIfTrue);
+                preparedStatement.setString(1, login_username.getText());
+                preparedStatement.setString(2, login_password.getText());
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("InformationMessage");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+                } else {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Incorrect Username/Password");
+                    alert.showAndWait();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     public void registrationButton() throws SQLException {
         if (register_account_username.getText().isEmpty() || register_account_password.getText().isEmpty()
-            || register_account_question.getSelectionModel().getSelectedItem() == null || register_account_answer.getText().isEmpty()
+                || register_account_question.getSelectionModel().getSelectedItem() == null || register_account_answer.getText().isEmpty()
         ) {
-            alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Message");
-            alert.setHeaderText(null);
-            alert.setContentText("Please fill all blank fields");
-            alert.showAndWait();
-        } else  {
-            SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:MM");
-            String date = sdf.format(new Date());
+            fillAllFieldError();
+        } else {
+//            SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:MM");
+//            String date = sdf.format(new Date());
+            Date date = new Date();
+            java.sql.Date _date = new java.sql.Date(date.getTime());
             String regData = "INSERT INTO Employee (username, password, question, answer, date) VALUES (?, ?, ?, ?, ?)";
             connection = Database.connectionDB();
+            System.out.println(isDBConnected());
+            try {
+                String checkUsername = "SELECT username FROM Employee WHERE username == '" + register_account_username.getText() + "'";
+                preparedStatement = connection.prepareStatement(checkUsername);
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText(register_account_username.getText() + " is already registered\nPlease Log in or click forgot password");
+                    alert.showAndWait();
 
-            try{
-                preparedStatement = connection.prepareStatement(regData);
-                preparedStatement.setString(1, register_account_username.getText());
-                preparedStatement.setString(2, register_account_password.getText());
-                preparedStatement.setString(3, register_account_question.getSelectionModel().getSelectedItem());
-                preparedStatement.setString(4, register_account_answer.getText());
-                preparedStatement.setString(5, date);
-                preparedStatement.executeUpdate();
-                alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Success");
-                alert.setHeaderText(null);
-                alert.setContentText("Operation was successful!\nPlease Log in");
+                    transitionLeft();
+                } else if (register_account_password.getText().length() < 8) {
+                    invalidPassword();
+                } else {
+                    preparedStatement = connection.prepareStatement(regData);
+                    preparedStatement.setString(1, register_account_username.getText());
+                    preparedStatement.setString(2, register_account_password.getText());
+                    preparedStatement.setString(3, register_account_question.getSelectionModel().getSelectedItem());
+                    preparedStatement.setString(4, register_account_answer.getText());
+                    preparedStatement.setString(5, String.valueOf(_date));
+                    preparedStatement.executeUpdate();
 
-                ButtonType closeButton = new ButtonType("Close", ButtonType.OK.getButtonData());
-                alert.getButtonTypes().setAll(closeButton);
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Success");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Operation was successful!\nPlease Log in");
 
-                alert.showAndWait();
-                register_account_username.setText("");
-                register_account_answer.setText("");
-                register_account_password.setText("");
-                register_account_question.getSelectionModel().clearSelection();
+                    ButtonType closeButton = new ButtonType("Close", ButtonType.OK.getButtonData());
+                    alert.getButtonTypes().setAll(closeButton);
 
-                translateTransition.setNode(side_form);
-                translateTransition.setToX(0);
-                translateTransition.setDuration(Duration.millis(1000));
-                translateTransition.play();
-                translateTransition.setOnFinished(e -> {
-                    side_already_have_an_account.setVisible(false);
-                    side_create_account_button.setVisible(true);
-                });
+                    alert.showAndWait();
+                    register_account_username.setText("");
+                    register_account_answer.setText("");
+                    register_account_password.setText("");
+                    register_account_question.getSelectionModel().clearSelection();
+
+                    transitionLeft();
 //                if (resultSet.next()) {
 //                    System.out.println("Sent");
 //                } else {
 //                    System.out.println("Error");
 //                }
+                }
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             } finally {
@@ -115,30 +156,57 @@ public class LoginController implements Initializable {
             return false;
         }
     }
+
     TranslateTransition translateTransition = new TranslateTransition();
+
     public void switchForm(ActionEvent event) {
         if (event.getSource() == side_create_account_button) {
-            translateTransition.setNode(side_form);
-            translateTransition.setToX(300);
-            translateTransition.setDuration(Duration.millis(1000));
-            translateTransition.play();
-            translateTransition.setOnFinished(e -> {
-                side_already_have_an_account.setVisible(true);
-                side_create_account_button.setVisible(false);
-            });
+            transitionRight();
 
         } else if (event.getSource() == side_already_have_an_account) {
-            translateTransition.setNode(side_form);
-            translateTransition.setToX(0);
-            translateTransition.setDuration(Duration.millis(1000));
-            translateTransition.play();
-            translateTransition.setOnFinished(e -> {
-                side_already_have_an_account.setVisible(false);
-                side_create_account_button.setVisible(true);
-            });
+            transitionLeft();
         }
 
     }
+
+    public void invalidPassword() {
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Message");
+        alert.setHeaderText(null);
+        alert.setContentText("Password must be more than 8 character.");
+        alert.showAndWait();
+    }
+
+    public void transitionLeft() {
+        translateTransition.setNode(side_form);
+        translateTransition.setToX(0);
+        translateTransition.setDuration(Duration.millis(1000));
+        translateTransition.play();
+        translateTransition.setOnFinished(e -> {
+            side_already_have_an_account.setVisible(false);
+            side_create_account_button.setVisible(true);
+        });
+    }
+
+    public void transitionRight() {
+        translateTransition.setNode(side_form);
+        translateTransition.setToX(300);
+        translateTransition.setDuration(Duration.millis(1000));
+        translateTransition.play();
+        translateTransition.setOnFinished(e -> {
+            side_already_have_an_account.setVisible(true);
+            side_create_account_button.setVisible(false);
+        });
+    }
+
+    public void fillAllFieldError() {
+        alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error Message");
+        alert.setHeaderText(null);
+        alert.setContentText("Please fill all blank fields");
+        alert.showAndWait();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         register_account_question.setItems(observableList);
@@ -149,5 +217,7 @@ public class LoginController implements Initializable {
                 throw new RuntimeException(e);
             }
         });
+        login_button.setOnAction(event -> loginAction());
     }
+
 }
